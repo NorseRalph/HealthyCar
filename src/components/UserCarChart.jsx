b6a70dc60e2429c22ebe7490680cc89238a51d88
RAUL AGHASHOV <kizilbashraul@gmail.com>
2023-11-23 21:26:23 +0100

    Stuff?

--- src/components/UserCarChart.jsx
+++ src/components/UserCarChart.jsx
@@ -10,118 +10,36 @@ const UserCarChart = () => {
   useEffect(() => {
     Chart.register(...registerables);
 
-    // Destroy the previous chart instances before creating new ones
-    if (lineChartRef.current) {
-      lineChartRef.current.destroy();
-    }
-    if (barChartRef.current) {
-      barChartRef.current.destroy();
-    }
-
-    const lineData = {
-      labels: currentRide.time,
-      datasets: [
-        {
-          label: "Speed",
-          data: currentRide.speed,
-          borderColor: "rgb(75, 192, 192)",
-          tension: 0.1,
-        },
-        {
-          label: "RPM",
-          data: currentRide.rpm,
-          borderColor: "rgb(192, 75, 75)",
-          tension: 0.1,
-        },
-        {
-          label: "Fuel Consumption",
-          data: currentRide.fuelConsumption,
-          borderColor: "rgb(75, 75, 192)",
-          tension: 0.1,
-        },
-        {
-          label: "Air Temperature",
-          data: currentRide.airTemperature,
-          borderColor: "rgb(192, 192, 75)",
-          tension: 0.1,
-        },
-        {
-          label: "Engine Temperature",
-          data: currentRide.engineTemperature,
-          borderColor: "rgb(75, 192, 75)",
-          tension: 0.1,
-        },
-      ],
-    };
-
-    const lineConfig = {
-      type: "line",
-      data: lineData,
-      options: {},
-    };
-
-    // Calculate averages for the bar chart
-    const averages = calculateAverages(allRides);
-
-    // Bar chart data for averages across all rides
-    const barData = {
-      labels: [
-        "Speed",
-        "RPM",
-        "Fuel Consumption",
-        "Air Temperature",
-        "Engine Temperature",
-      ],
-      datasets: [
-        {
-          label: "Average",
-          data: [
-            averages.speed,
-            averages.rpm,
-            averages.fuelConsumption,
-            averages.airTemperature,
-            averages.engineTemperature,
+    // Function to create a line chart
+    const createLineChart = (canvasId, label, data, borderColor) => {
+      const chartCanvas = document.getElementById(canvasId).getContext("2d");
+      chartsRef.current[canvasId] = new Chart(chartCanvas, {
+        type: "line",
+        data: {
+          labels: currentRide.time,
+          datasets: [
+            {
+              label: label,
+              data: data,
+              borderColor: borderColor,
+              tension: 0.1,
+            },
           ],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-          ],
-          borderWidth: 1,
         },
-      ],
+        options: {},
+      });
     };
 
-    const barConfig = {
-      type: "bar",
-      data: barData,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    };
+    // Destroy previous chart instances
+    Object.values(chartsRef.current).forEach((chart) => chart.destroy());
+    chartsRef.current = {};
 
-    const lineChartCanvas = document
-      .getElementById("lineUserCarChart")
-      .getContext("2d");
-    lineChartRef.current = new Chart(lineChartCanvas, lineConfig);
-
-    const barChartCanvas = document
-      .getElementById("barUserCarChart")
-      .getContext("2d");
-    barChartRef.current = new Chart(barChartCanvas, barConfig);
+    // Create line charts for each attribute
+    createLineChart("speedChart", "Speed", currentRide.speed, "rgb(75, 192, 192)");
+    createLineChart("rpmChart", "RPM", currentRide.rpm, "rgb(192, 75, 75)");
+    createLineChart("fuelChart", "Fuel Consumption", currentRide.fuelConsumption, "rgb(75, 75, 192)");
+    createLineChart("airTempChart", "Air Temperature", currentRide.airTemperature, "rgb(192, 192, 75)");
+    createLineChart("engineTempChart", "Engine Temperature", currentRide.engineTemperature, "rgb(75, 192, 75)");
 
     // Cleanup function
     return () => {
